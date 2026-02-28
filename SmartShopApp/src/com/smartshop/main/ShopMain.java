d3735fa605bebe38227258c953710930f0001203
sonamjadhav12 <shonikjadhav@gmail.com>
2026-02-28 22:31:06 +0530

    Added new changes

--- SmartShopApp/src/com/smartshop/main/ShopMain.java
+++ SmartShopApp/src/com/smartshop/main/ShopMain.java
@@ -2,6 +2,8 @@ package com.smartshop.main;
 
 import java.util.Scanner;
 
+import com.smartshop.classes.InsertOperation_UsersTable;
+
 public class ShopMain {
 	public static void selectUser() {
 		
@@ -55,7 +57,53 @@ public class ShopMain {
 		switch(choice) {
 		
 		case 1:
-		case 2:
+			System.out.println("Welcome to User Registration");
+			System.out.println("Enter First Name >>");
+			String firstname=scanner.next();
+			System.out.println("Enter Last Name >>");
+			String lastname=scanner.next();		
+			String username;
+			while(true){
+			    System.out.println("Enter UserName >>");
+			    username=scanner.next();
+			    System.out.println("Checking for duplicate username...");
+			    if(InsertOperation_UsersTable.isUsernameExists(username)){
+			        System.out.println("Username already exists! Try again.");
+			    }
+			    else{
+			        break;
+			    }
+			}
+			System.out.println("Enter Password >>");
+			String password=scanner.next();
+			System.out.println("Enter City >>");
+			String city=scanner.next();
+			System.out.println("Enter Email ID >>");
+			String emailId=scanner.next();
+			System.out.println("Enter Mobile Number >>");
+			double mobileNumber=scanner.nextDouble();
+			
+			InsertOperation_UsersTable.InsertUser(firstname,lastname,username,password,city,emailId,mobileNumber);
+			System.out.println("Successfully registered");			
+			
+		case 2:		
+			System.out.println("Welcome to Login page...");
+			while(true){
+				System.out.println("Enter UserName >>");
+			    username=scanner.next();
+				System.out.println("Enter Password >>");
+				password=scanner.next();
+				System.out.println("Verifying credentials...");
+			    if(InsertOperation_UsersTable.isUsernameExists(username)){
+			    	if(InsertOperation_UsersTable.isPasswordExists(password)){
+				        System.out.println("Successfully logged in.");
+				        break;
+				    }
+			    }
+			    else{
+			    	System.out.println("Wrong username or password! Try again.");
+			    }
+			}
 		case 3:
 		case 4:
 		case 5:
